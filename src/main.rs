a47c478e08ac01ccd8708cd4f88a1fd1110ef5b4
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-10-09 16:39:34 +0200

    Working on supplying n by argument.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 fn fib(n: u64) -> u64 {
-	if n < 2 {
-		return n
-	}
-	let mut previous = 0u64;
-	let mut current = 1u64;
-	let mut tmp: u64;
-	for _ in 1..n {
-		tmp = current;
-		current = current + previous;
-		previous = tmp;
-	}
-	current
+    if n < 2 {
+        return n;
+    }
+    let mut previous = 0u64;
+    let mut current = 1u64;
+    let mut tmp: u64;
+    for _ in 1..n {
+        tmp = current;
+        current = current + previous;
+        previous = tmp;
+    }
+    current
 }
 
 fn main() {
-	println!("{}", fib(1_000_000_000_000));
+    let n = std::env::args()
+        .nth(1)
+        .expect("Usage: fib [n]")
+        .parse()
+        .expect("Could not parse n");
+    println!("{}", fib(n));
 }
