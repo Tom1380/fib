9c6973c096e94c0d1ea140563547e675abd0fbcf
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2020-10-09 16:49:52 +0200

    Using BigInt, it now supports numbers higher than 93-

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use num_bigint::BigUint;
+use num_traits::{One, Zero};
+use std::mem::replace;
+
 fn fib(n: u64) -> u64 {
     if n < 2 {
         return n;
@@ -13,11 +17,27 @@ fn fib(n: u64) -> u64 {
     current
 }
 
+// Calculate large fibonacci numbers.
+fn fib_bigint(n: u64) -> BigUint {
+    let mut f0: BigUint = Zero::zero();
+    let mut f1: BigUint = One::one();
+    for _ in 0..n {
+        let f2 = f0 + &f1;
+        // This is a low cost way of swapping f0 with f1 and f1 with f2.
+        f0 = replace(&mut f1, f2);
+    }
+    f0
+}
+
 fn main() {
     let n = std::env::args()
         .nth(1)
         .expect("Usage: fib [n]")
         .parse()
         .expect("Could not parse n");
-    println!("{}", fib(n));
+    if n > 93 {
+        println!("{}", fib_bigint(n))
+    } else {
+        println!("{}", fib(n))
+    }
 }
